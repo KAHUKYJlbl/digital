616e0b5b6a81c683be95279214d6758dae2f9c77
Iurii Pototskii <iuriipototskii2gmail.com>
2023-11-02 04:11:50 +0100

    feat: message validation

--- src/view/modal/mail/mail-modal-view.ts
+++ src/view/modal/mail/mail-modal-view.ts
@@ -3,6 +3,7 @@ import ButtonView from '../../button/button-view.ts';
 import { createMailModalTemplate } from './mail-modal-template.ts';
 import { MessageType } from '../../../model/types.ts';
 import AbstractView from '../../../abstract/view/view.ts';
+import { validation } from '../../../utils/validation.ts';
 
 const INIT_STATE: MessageType = {
   name: "",
@@ -14,6 +15,7 @@ export default class MailModalView extends AbstractView {
   #buttonId = "modal-submit";
   #callback: (data: MessageType) => void;
   _state: MessageType;
+  _error = false;
 
   constructor ( callback: (data: MessageType) => void ) {
     super();
@@ -29,7 +31,7 @@ export default class MailModalView extends AbstractView {
   };
 
   _addHandlers = () => {
-    this.element?.querySelector(`#${this.#buttonId}`)?.addEventListener('click', () => this.#callback(this._state));
+    this.element?.querySelector(`#${this.#buttonId}`)?.addEventListener('click', this.#formSubmitHandler);
 
     // input fields
     this.element?.querySelector('#name')?.addEventListener('input', this.#inputNameHandler);
@@ -37,34 +39,72 @@ export default class MailModalView extends AbstractView {
     this.element?.querySelector('#message')?.addEventListener('input', this.#inputMessageHandler);
   };
 
+  #formSubmitHandler = () => {
+    if ( !this._error ) {
+      this.#callback(this._state);
+    }
+  }
+
   #inputNameHandler = (e: Event) => {
     e.preventDefault();
+    const value = (e.target as HTMLInputElement).value;
+
+    if ( !validation('name', value) ) {
+      document.querySelector('#name')?.classList.add('error');
+      this._setError(true);
+      return;
+    }
+
+    document.querySelector('#name')?.classList.remove('error');
+    this._setError(false);
 
     this._setState({
       ...this._state,
-      name: he.encode((e.target as HTMLInputElement).value),
+      email: he.encode(value),
     });
   };
 
   #inputEmailHandler = (e: Event) => {
     e.preventDefault();
+    const value = (e.target as HTMLInputElement).value;
+
+    if ( validation('email', value) ) {
+      document.querySelector('#email')?.classList.add('error');
+      this._setError(true);
+    }
+
+    document.querySelector('#email')?.classList.remove('error');
+    this._setError(false);
 
     this._setState({
       ...this._state,
-      email: he.encode((e.target as HTMLInputElement).value),
+      email: he.encode(value),
     });
   };
 
   #inputMessageHandler = (e: Event) => {
     e.preventDefault();
+    const value = (e.target as HTMLInputElement).value;
+
+    if ( validation('message', value) ) {
+      document.querySelector('#message')?.classList.add('error');
+      this._setError(true);
+    }
+
+    document.querySelector('#message')?.classList.remove('error');
+    this._setError(false);
 
     this._setState({
       ...this._state,
-      message: he.encode((e.target as HTMLInputElement).value),
+      email: he.encode(value),
     });
   };
 
   get template() {
     return createMailModalTemplate( (new ButtonView(`Let's Talk`, this.#buttonId)).template );
   };
+
+  _setError (error: boolean) {
+    this._error = error;
+  }
 }
